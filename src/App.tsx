8bec5e41e1b637949d01e1d43373cfde5ae7df3e
saeid-tavazani <saeidtavazani@gmail.com>
2024-07-15 16:22:00 +0330

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import SignInForm from "./page/auth/forms/signinForm";
+import SignUpForm from "./page/auth/forms/signUpForm";
+import { Home } from "./page/_root/page";
+import AuthLayout from "./page/auth/authLayout";
+import RootLayout from "./page/_root/rootLayout";
 const App = () => {
   return (
     <Routes>
-      <Route path="/sign-in" />
-      <Route index />
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+      </Route>
+      <Route element={<RootLayout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
   );
 };
