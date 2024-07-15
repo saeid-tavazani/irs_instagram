ff4515c3c9476514a5e6225350a65394d4ad80d4
saeid-tavazani <saeidtavazani@gmail.com>
2024-07-15 16:08:36 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router-dom";
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/sign-in" />
+      <Route index />
+    </Routes>
+  );
+};
+export default App;
